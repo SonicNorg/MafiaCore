24380a50f64273aef5a2644b1f4cafe5d51174f7
pavel.krizhanovskiy <pavel.proger@gmail.com>
2016-04-07 12:06:36 +0600

    close #1 Class Player made truly immutable

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,17 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 /**
+ * Class for Mafia player
  * Created by Norg on 07.04.2016.
  */
-public class Player {
-    boolean sex; //false is female
-    int number;
-    PlayerState state;
-    String name;
-    Role role;
-    int fault;
+public final class Player {
+    private final boolean sex; //false is female
+    private final int number;
+    private final PlayerState state;
+    private final String name;
+    private final Role role;
+    private final int fault;
 
     void speech() {
         speech(60);
@@ -32,5 +36,17 @@ public class Player {
 
     public Player (int nmbr, boolean sx, Role rl) {
         this(nmbr, "Игрок", sx, rl, PlayerState.IN_GAME, 0);
+    }
+
+    public Player withNewState(@NotNull PlayerState state) {
+        return new Player(this.number, this.name, this.sex, this.role, state, this.fault);
+    }
+
+    public Player addFault() {
+        return new Player(this.number, this.name, this.sex, this.role, this.state, this.fault+1);
+    }
+
+    public int faults() {
+        return this.fault;
     }
 }
